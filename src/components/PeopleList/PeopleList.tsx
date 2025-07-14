e39153cd2e5c7c1558e48fe1a844a56bcf97a927
godlikeAlex <godlikedesigner@gmail.com>
2025-07-14 14:11:29 +0500

    feat: add ItemComponent

--- src/components/PeopleList/PeopleList.tsx
+++ src/components/PeopleList/PeopleList.tsx
@@ -1,5 +1,6 @@
 import { Component, type ReactNode } from "react";
 import type { People } from "../../types/People";
+import PeopleListItem from "./PeopleListItem";
 
 type Props = {
   peoples: People[];
@@ -8,21 +9,7 @@ type Props = {
 export default class PeopleList extends Component<Props> {
   render(): ReactNode {
     const peopleRows = this.props.peoples.map((people) => (
-      <tr
-        key={people.name}
-        className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
-      >
-        <th
-          scope="row"
-          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-        >
-          {people.name}
-        </th>
-        <td className="px-6 py-4">{people.height}</td>
-        <td className="px-6 py-4">{people.birth_year}</td>
-        <td className="px-6 py-4">{people.gender}</td>
-        <td className="px-6 py-4">{people.mass}</td>
-      </tr>
+      <PeopleListItem key={people.name} people={people} />
     ));
 
     return (
