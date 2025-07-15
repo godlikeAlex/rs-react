1a2e8c60a804fd44f34d522528a8bf03601ffc1f
godlikeAlex <godlikedesigner@gmail.com>
2025-07-15 18:19:50 +0500

    refactor: add classnames to input field

--- src/components/SearchControl/SearchControl.tsx
+++ src/components/SearchControl/SearchControl.tsx
@@ -1,5 +1,6 @@
 import { Component, type FormEvent, type ReactNode } from "react";
 import { Button } from "../Button";
+import classNames from "classnames";
 
 type Props = {
   onChange: (text: string) => void;
@@ -30,7 +31,13 @@ export default class SearchControl extends Component<Props> {
           placeholder={this.props.placeholder}
           value={value}
           onChange={(e) => onChange(e.target.value)}
-          className="bg-gray-50 border border-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 px-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className={classNames(
+            "block w-full p-1.5 px-4",
+            "text-gray-900 text-sm",
+            "dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white",
+            "bg-gray-50 border border-gray-200  rounded-lg",
+            "focus:ring-blue-500 focus:border-blue-500 dark:focus:ring-blue-500 dark:focus:border-blue-500",
+          )}
         />
 
         <Button className="flex-1/3" disabled={disabled}>
