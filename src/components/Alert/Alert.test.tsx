550ed090d4ff4cf803c61db196691dac410b7416
godlikeAlex <godlikedesigner@gmail.com>
2025-07-17 18:14:46 +0500

    feat: add tests for Alert component

---
+++ src/components/Alert/Alert.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from "@testing-library/react";
+import { it, describe, expect } from "vitest";
+import Alert from "./Alert";
+
+describe("Alert Component", () => {
+  it("should render with correct children", () => {
+    expect.hasAssertions();
+
+    const alertMessage = "Thank you.";
+
+    render(<Alert variant="success">{alertMessage}</Alert>);
+
+    expect(screen.getByText(alertMessage)).toBeInTheDocument();
+  });
+
+  it("should render success variant", () => {
+    expect.hasAssertions();
+
+    render(<Alert variant="success">example</Alert>);
+
+    expect(screen.getByRole("alert")).toHaveClass(
+      "text-green-600",
+      "dark:text-green-400",
+    );
+  });
+
+  it("should render danger variant", () => {
+    expect.hasAssertions();
+
+    render(<Alert variant="danger">example</Alert>);
+
+    expect(screen.getByRole("alert")).toHaveClass(
+      "text-red-800",
+      "dark:text-red-400",
+    );
+  });
+});
