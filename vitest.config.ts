f1bef6ebff34512cbb931cc029180cb03484e287
godlikeAlex <godlikedesigner@gmail.com>
2025-07-17 18:14:25 +0500

    chore: update include path for coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(
         setupFiles: "./src/tests/setupTests.ts",
         coverage: {
           provider: "v8",
-          include: ["./src/**/*.{js,jsx,ts,tsx}"],
+          include: ["src/**/*.{js,jsx,ts,tsx}"],
           exclude: [
             "./src/**/*.test.{js,jsx,ts,tsx}",
             "./src/**/*.spec.{js,jsx,ts,tsx}",
