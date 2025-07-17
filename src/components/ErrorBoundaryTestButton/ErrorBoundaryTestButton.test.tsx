53e57b9de904fa6ae908faffb6f6ba701a419179
godlikeAlex <godlikedesigner@gmail.com>
2025-07-18 00:52:32 +0500

    feat: add tests for ErrorBoundaryTestButton

---
+++ src/components/ErrorBoundaryTestButton/ErrorBoundaryTestButton.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import ErrorBoundaryTestButton from "./ErrorBoundaryTestButton";
+import userEvent from "@testing-library/user-event";
+import { ErrorBoundary } from "@/components";
+
+describe("ErrorBoundaryTestButton Component", () => {
+  it("should render button", () => {
+    expect.hasAssertions();
+
+    render(<ErrorBoundaryTestButton />);
+
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  it("should throw render error when click button", async () => {
+    expect.hasAssertions();
+
+    const user = userEvent.setup();
+
+    render(
+      <ErrorBoundary>
+        <ErrorBoundaryTestButton />
+      </ErrorBoundary>,
+    );
+
+    await user.click(screen.getByRole("button"));
+
+    expect(
+      screen.getByRole("heading", {
+        name: "An unexpected error has occurred.",
+      }),
+    ).toBeInTheDocument();
+  });
+});
