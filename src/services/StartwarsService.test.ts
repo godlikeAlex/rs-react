ff807e2dc67a1a959fdcb7fc7e3cbc171035862c
godlikeAlex <godlikedesigner@gmail.com>
2025-07-18 14:13:32 +0500

    feat: add tests for service

---
+++ src/services/StartwarsService.test.ts
@@ -0,0 +1,28 @@
+import { it, describe, vi, expect } from "vitest";
+import * as apiModule from "@/core/api";
+import StarWarsService, { BASE_URL } from "./StarwarsService";
+
+vi.mock("@/core/api");
+
+describe("StarwarsService", () => {
+  it("should call api with correct searchTerm", async () => {
+    expect.hasAssertions();
+
+    const searchTerm = "Luke";
+
+    const mockResponse = {
+      count: 1,
+      results: [{ name: "Luke Skywalker" }],
+    };
+
+    const apiMock = vi.spyOn(apiModule, "api").mockResolvedValue(mockResponse);
+
+    const result = await StarWarsService.search(searchTerm);
+
+    expect(apiMock).toHaveBeenCalledWith(
+      `${BASE_URL}/people?search=${searchTerm}`,
+    );
+
+    expect(result.results).toHaveLength(mockResponse.count);
+  });
+});
