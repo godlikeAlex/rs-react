62ee23c59f3dd95dc9e1fe03a0e5ad82f0708c9f
godlikeAlex <godlikedesigner@gmail.com>
2025-07-18 13:55:32 +0500

    chore: add main.tsx to exclude from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -16,6 +16,7 @@ export default defineConfig(
             "./src/**/*.test.{js,jsx,ts,tsx}",
             "./src/**/*.spec.{js,jsx,ts,tsx}",
             "./src/index.{js,jsx,ts,tsx}",
+            "./src/main.tsx",
             "./src/tests/setupTests.{js,ts}",
             "./src/**/*.d.ts",
           ],
