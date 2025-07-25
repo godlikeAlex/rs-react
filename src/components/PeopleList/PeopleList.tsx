deaf351fff7ef195a92d2757e86ac6390125c778
godlikeAlex <godlikedesigner@gmail.com>
2025-07-25 23:13:10 +0500

    refactor: migrate PeopleList to functional component

--- src/components/PeopleList/PeopleList.tsx
+++ src/components/PeopleList/PeopleList.tsx
@@ -1,4 +1,3 @@
-import { Component, type ReactNode } from "react";
 import type { People } from "@/types/People";
 import PeopleListItem from "./PeopleListItem";
 
@@ -6,37 +5,35 @@ type Props = {
   peoples: People[];
 };
 
-export default class PeopleList extends Component<Props> {
-  render(): ReactNode {
-    const peopleRows = this.props.peoples.map((people) => (
-      <PeopleListItem key={people.name} people={people} />
-    ));
+export default function PeopleList({ peoples }: Props) {
+  const peopleRows = peoples.map((people) => (
+    <PeopleListItem key={people.name} people={people} />
+  ));
 
-    return (
-      <div className="relative overflow-x-auto">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Height
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Birth Year
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Gender
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Mass
-              </th>
-            </tr>
-          </thead>
-          <tbody>{peopleRows}</tbody>
-        </table>
-      </div>
-    );
-  }
+  return (
+    <div className="relative overflow-x-auto">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Height
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Birth Year
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Gender
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Mass
+            </th>
+          </tr>
+        </thead>
+        <tbody>{peopleRows}</tbody>
+      </table>
+    </div>
+  );
 }
