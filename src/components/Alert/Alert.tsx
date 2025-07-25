a59ae58629e440d4df9f8d085f21f6bc4063ca0a
godlikeAlex <godlikedesigner@gmail.com>
2025-07-25 16:20:54 +0500

    refactor: migrate Alert to functional component

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,27 +1,23 @@
 import classNames from "classnames";
-import { Component, type ReactNode } from "react";
+import { type PropsWithChildren } from "react";
 
 type Props = {
-  children: ReactNode;
   variant: "danger" | "success";
 };
 
-export default class Alert extends Component<Props> {
-  render(): ReactNode {
-    return (
-      <div
-        className={classNames(
-          "p-4 text-sm  rounded-lg bg-red-50 dark:bg-gray-800 ",
-          {
-            "text-red-800 dark:text-red-400": this.props.variant === "danger",
-            "text-green-600 dark:text-green-400":
-              this.props.variant === "success",
-          },
-        )}
-        role="alert"
-      >
-        {this.props.children}
-      </div>
-    );
-  }
+export default function Alert({ variant, children }: PropsWithChildren<Props>) {
+  return (
+    <div
+      className={classNames(
+        "p-4 text-sm  rounded-lg bg-red-50 dark:bg-gray-800 ",
+        {
+          "text-red-800 dark:text-red-400": variant === "danger",
+          "text-green-600 dark:text-green-400": variant === "success",
+        }
+      )}
+      role="alert"
+    >
+      {children}
+    </div>
+  );
 }
