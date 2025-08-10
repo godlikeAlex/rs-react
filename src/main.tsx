1f3153fb387a86db760d418164a8289ba42c0a0c
godlikeAlex <godlikedesigner@gmail.com>
2025-08-10 11:25:20 +0500

    chore: add tanstack/react-query

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,20 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ErrorBoundary } from "@/components";
 import { ThemeProvider } from "@/contexts/ThemeContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./main.css";
+
+export const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ErrorBoundary>
       <ThemeProvider>
         <BrowserRouter>
-          <App />
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
         </BrowserRouter>
       </ThemeProvider>
     </ErrorBoundary>
