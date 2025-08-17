06e56023755f6ee357866fcf09e6406a475762f3
godlikeAlex <godlikedesigner@gmail.com>
2025-08-17 22:11:49 +0500

    refactor: make nossr themprovider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 import { type ReactNode } from "react";
-import { ThemeProvider } from "@/contexts/ThemeContext";
+import dynamic from "next/dynamic";
+
+const ThemeProviderWithoutSSR = dynamic(
+  () => import("@/contexts/ThemeContext/ThemeContextProvider"),
+  { ssr: false }
+);
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return <ThemeProviderWithoutSSR>{children}</ThemeProviderWithoutSSR>;
 }
