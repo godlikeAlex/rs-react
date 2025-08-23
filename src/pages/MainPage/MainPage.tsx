3b2150354bf47889fb3446d93dc6fd8ce58d13ea
godlikeAlex <godlikedesigner@gmail.com>
2025-08-23 14:05:47 +0500

    feat: add basic content for main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,20 @@
+import { Button } from "@/components";
+import clsx from "clsx";
+
 export default function MainPage() {
-  return <h1>Main Page</h1>;
+  return (
+    <div
+      className={clsx(
+        "w-full h-screen p-5",
+        "bg-neutral-50",
+        "flex items-center justify-center flex-col"
+      )}
+    >
+      <h1 className="text-7xl">React Forms</h1>
+      <div className="flex gap-5 mt-8">
+        <Button>Controlled Form</Button>
+        <Button variant="ghost">Uncontrolled Form</Button>
+      </div>
+    </div>
+  );
 }
