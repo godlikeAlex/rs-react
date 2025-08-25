dc4dae295011a5d8d0fc971be8a80cd54d1c22da
godlikeAlex <godlikedesigner@gmail.com>
2025-08-25 05:00:00 +0500

    feat: add tests for Input

---
+++ src/components/Input/Input.test.tsx
@@ -0,0 +1,21 @@
+import { describe, expect, it } from "vitest";
+import Input from "./Input";
+import { render, screen } from "@testing-library/react";
+
+describe("Select Component", () => {
+  it("should render autocomplete list", () => {
+    render(<Input autoCompleteList={["example"]} />);
+
+    expect(screen.getByText("example")).toBeInTheDocument();
+  });
+
+  it("should render label", () => {
+    const label = "ExampleLabel";
+
+    render(<Input label={label} />);
+
+    console.log(screen.logTestingPlaygroundURL());
+
+    expect(screen.getByText(label)).toBeInTheDocument();
+  });
+});
