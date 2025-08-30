05f588c3e4f578dc12da4d8a2e008da602b9a8a0
godlikeAlex <godlikedesigner@gmail.com>
2025-08-30 13:30:19 +0500

    feat: add basic layout and logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <h2 className="text-3xl">
+      <span className="text-blue-700">C</span>
+      <span className="text-red-700">O2</span> Data
+    </h2>
+  );
+}
