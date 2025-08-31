963ca21b487d21b1dd66d88942cc1074d23f3d85
godlikeAlex <godlikedesigner@gmail.com>
2025-08-31 21:59:50 +0500

    feat: add highliht to columns

--- src/widgets/Co2Table/components/TableColumn/TableColumn.tsx
+++ src/widgets/Co2Table/components/TableColumn/TableColumn.tsx
@@ -1,7 +1,47 @@
+import clsx from "clsx";
+import { useEffect, useRef, useState } from "react";
+
 interface Props {
   value?: string | number;
+  highlightOnUpdate?: boolean;
 }
 
-export default function TableColumn({ value = "N/A" }: Props) {
-  return <td className="p-4">{value}</td>;
+export default function TableColumn({
+  value = "N/A",
+  highlightOnUpdate,
+}: Props) {
+  const [isHighlighted, setIsHighlighted] = useState(false);
+  const isFirstRender = useRef(true);
+  const prevValueRef = useRef(value);
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout | undefined;
+
+    if (!highlightOnUpdate) return;
+
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+
+    if (prevValueRef.current !== value) {
+      setIsHighlighted(true);
+      timeout = setTimeout(() => setIsHighlighted(false), 800);
+    }
+
+    prevValueRef.current = value;
+
+    return () => timeout && clearTimeout(timeout);
+  }, [value, highlightOnUpdate]);
+
+  return (
+    <td
+      className={clsx(
+        "p-4 transition-colors duration-700",
+        isHighlighted ? "bg-yellow-300" : "bg-transparent"
+      )}
+    >
+      {value}
+    </td>
+  );
 }
--- src/widgets/Co2Table/components/TableRowCountry/TableRowCountry.tsx
+++ src/widgets/Co2Table/components/TableRowCountry/TableRowCountry.tsx
@@ -1,6 +1,7 @@
 import type { Country } from "@/types/Country";
 import CountryService from "@/widgets/Co2Table/services/CountryService";
 import { TableColumn } from "@/widgets/Co2Table/components";
+import useTable from "@/widgets/Co2Table/hooks/useTable";
 
 interface Props {
   country: Country & { name: string };
@@ -8,6 +9,8 @@ interface Props {
 }
 
 export default function TableRowCountry({ country, selectedYear }: Props) {
+  const { visibleColumns } = useTable();
+
   const currentCountryEntryData = CountryService.retrieveEntryData(
     country.data,
     selectedYear
@@ -17,10 +20,27 @@ export default function TableRowCountry({ country, selectedYear }: Props) {
     <tr className="border-b border-stone-200 text-xs">
       <TableColumn value={country.iso_code} />
       <TableColumn value={country.name} />
-      <TableColumn value={currentCountryEntryData?.population} />
-      <TableColumn value={currentCountryEntryData?.year} />
-      <TableColumn value={currentCountryEntryData?.nitrous_oxide} />
-      <TableColumn value={currentCountryEntryData?.nitrous_oxide_per_capita} />
+      <TableColumn
+        value={currentCountryEntryData?.population}
+        highlightOnUpdate
+      />
+      <TableColumn value={currentCountryEntryData?.year} highlightOnUpdate />
+      <TableColumn
+        value={currentCountryEntryData?.nitrous_oxide}
+        highlightOnUpdate
+      />
+      <TableColumn
+        value={currentCountryEntryData?.nitrous_oxide_per_capita}
+        highlightOnUpdate
+      />
+
+      {visibleColumns.map((columnName) => (
+        <TableColumn
+          key={columnName}
+          value={currentCountryEntryData?.[columnName]}
+          highlightOnUpdate
+        />
+      ))}
     </tr>
   );
 }
