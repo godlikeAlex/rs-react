7405439681987da97932127ab68165017989910c
godlikeAlex <godlikedesigner@gmail.com>
2025-08-31 18:05:48 +0500

    feat: render visible columns in header

--- src/widgets/Co2Table/components/TableHeader/TableHeader.tsx
+++ src/widgets/Co2Table/components/TableHeader/TableHeader.tsx
@@ -1,12 +1,22 @@
 import { DEFAULT_COLUMNS } from "@/widgets/Co2Table/constants/defaultColumns";
 import TableHeadColumn from "./TableHeadColumn";
+import useTable from "@/widgets/Co2Table/hooks/useTable";
+import { transformColumnNameToHuman } from "@/uitls/utils";
 
 export default function TableHeader() {
+  const { visibleColumns } = useTable();
+
   return (
     <thead className="text-sm uppercase bg-stone-50 w-full">
       <tr>
         {DEFAULT_COLUMNS.map(({ label }) => (
           <TableHeadColumn key={label} name={label} />
+        ))}
+        {visibleColumns.map((label) => (
+          <TableHeadColumn
+            key={label}
+            name={transformColumnNameToHuman(label)}
+          />
         ))}
       </tr>
     </thead>
